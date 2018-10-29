f31071067920dd494105acfb1c80c832659b306f
Cristian <cusumdt@gmail.com>
2018-10-29 20:26:38 -0300

    Arreglo de creditos

--- Dragons-SideScrolling/src/screen/Credits/credits.cpp
+++ Dragons-SideScrolling/src/screen/Credits/credits.cpp
@@ -9,13 +9,10 @@ namespace Game
 	namespace initCredits
 	{
 		Texture2D fond;
-		Rectangle recExit;
-		Texture2D exit;
-		Texture2D negativeExit;
 		Texture2D menu;
 		Texture2D negativeMenu;
 		Rectangle recMenu;
-		static int positionY = screenHeight / 4;
+		static int positionY = screenHeight- screenHeight/100*60;
 		static bool menuButtonAnimationOn;
 		static bool exitButtonAnimationOn;
 		static bool firstInit = true;
@@ -28,11 +25,8 @@ namespace Game
 				firstInit = false;
 				menu = LoadTexture("res/boton_menu.png");
 				negativeMenu = LoadTexture("res/boton_menu2.png");
-				exit = LoadTexture("res/exit.png");
-				negativeExit = LoadTexture("res/exit2.png");
 				fond = LoadTexture("res/menu.png");
-				recMenu = { (float)halfScreenWidth - menu.width / 2,(float)screenHeight / 2 ,(float)menu.width,(float)menu.height };
-				recExit = { (float)halfScreenWidth - exit.width / 2,(float)screenHeight / 2 + exit.height + 5,(float)exit.width,(float)exit.height };
+				recMenu = { (float)halfScreenWidth - menu.width / 2,(float)screenHeight- screenHeight / 100 *43 ,(float)menu.width,(float)menu.height };
 			}
 		}
 		void UpdateCredits()
@@ -50,38 +44,17 @@ namespace Game
 			{
 				menuButtonAnimationOn = true;
 			}
-			if (CheckCollisionPointRec(GetMousePosition(), recExit))
-			{
-				exitButtonAnimationOn = false;
-
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-				{
-					Game::endGame = !Game::endGame;
-				}
-			}
-			else
-			{
-				exitButtonAnimationOn = true;
-			}
 		}
 		void DrawCredits() 
 		{
 			DrawTexture(fond, 0, 0, WHITE);
 			if (menuButtonAnimationOn)
 			{
-				DrawTexture(menu, halfScreenWidth - menu.width / 2,screenHeight / 2, WHITE);
+				DrawTexture(menu, halfScreenWidth - menu.width / 2, screenHeight - screenHeight / 100 * 43, WHITE);
 			}
 			else
 			{
-				DrawTexture(negativeMenu, halfScreenWidth  - negativeMenu.width / 2, screenHeight / 2, WHITE);
-			}
-			if (exitButtonAnimationOn)
-			{
-				DrawTexture(exit, halfScreenWidth - exit.width / 2, screenHeight / 2 + exit.height + 5, WHITE);
-			}
-			else
-			{
-				DrawTexture(negativeExit, halfScreenWidth - exit.width / 2,screenHeight / 2 + exit.height + 5, WHITE);
+				DrawTexture(negativeMenu, halfScreenWidth  - negativeMenu.width / 2, screenHeight - screenHeight / 100 * 43, WHITE);
 			}
 			DrawText("Programmer: Cusumano Cristian", halfScreenWidth - (MeasureText("Programmer: Cusumano Cristian", fontSize) / 2), positionY , fontSize, GOLD);
 			DrawText("Music and Efects By: Cusumano Cristian", halfScreenWidth - (MeasureText("Music and Efects By: Cusumano Cristian", fontSize) / 2), positionY + fontSize*2 + 5, fontSize, GOLD);
